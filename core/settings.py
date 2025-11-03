d07d05bb7a45d61159db9fa679e83c08a3833812
djumanov <djumanovdev@gmail.com>
2025-11-03 17:10:59 +0500

    startapp

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
